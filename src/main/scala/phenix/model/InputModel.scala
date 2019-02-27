7a86a8f8fc7741ef3079bc9b8c5933faeaaaaeb4
Matthieu SAUBOUA-BENELUZ <msauboua@ippon.fr>
2019-02-27 14:31:54 +0100

    feat: ajout récupération des metadata des fichiers

--- src/main/scala/phenix/model/InputModel.scala
+++ src/main/scala/phenix/model/InputModel.scala
@@ -5,3 +5,8 @@ import java.util.Date
 case class Transaction(transactionId: Int, date: Date, shopUuid: String, productId: Int, quantity: Int)
 case class Product(productId: Int, price: Double)
 
+case class TransactionFileMetaData(date: Date)
+case class ProductFileMetaData(shopUuid: String, date: Date)
+
+case class Transactions(transactions: Stream[Transaction], metaData: TransactionFileMetaData)
+case class Products(products: Stream[Product], metaData: ProductFileMetaData)
--- src/main/scala/phenix/service/FileService.scala
+++ src/main/scala/phenix/service/FileService.scala
@@ -1,18 +1,15 @@
 package phenix.service
 
 import java.nio.file.Path
-import java.text.SimpleDateFormat
 
 import better.files.File
 import com.typesafe.scalalogging.LazyLogging
-import phenix.model.{Product, Transaction}
 
 import scala.util.{Failure, Success, Try}
 
 trait fileProducer extends LazyLogging {
 
 }
-
 
 trait FileIngester extends LazyLogging {
   def ingestFile(filePath: Path): Try[Stream[String]] = {
@@ -26,41 +23,3 @@ trait FileIngester extends LazyLogging {
   }
 }
 
-trait Marshaller[T] extends FileIngester {
-  val CARREFOUR_HORIZONTAL_SEPARATOR = """\|"""
-  val CARREFOUR_DATE_FORMAT = new SimpleDateFormat("yyyyMMdd'T'HHmmssZ")
-
-  def marshallFile(filePath: Path, deserializeFunction: String => T): Stream[T] = {
-    ingestFile(filePath) match { // TODO This should not be done by the marshaller but by a wrapper object around it
-      case Success(lines) => lines.map(line => deserializeFunction(line))
-      case Failure(throwable) =>
-        logger.error(throwable.getMessage)
-        Stream.empty
-    }
-  }
-
-  def marshallLines(filePath: Path): Stream[T] = {
-    marshallFile(filePath, marshallLineString)
-  }
-
-  // NOTE: For now this method should not consider any possible errors in the source data
-  // But It should be done in the future
-  def marshallLineString(line: String): T
-}
-
-object TransactionMarshaller extends Marshaller[Transaction] with LazyLogging {
-  override def marshallLineString(line: String): Transaction = {
-    line.split(CARREFOUR_HORIZONTAL_SEPARATOR) match {
-      case Array(transactionId, dateAsString, shopUuid, productId, quantity) =>
-        Transaction(transactionId.toInt, CARREFOUR_DATE_FORMAT.parse(dateAsString), shopUuid, productId.toInt, quantity.toInt)
-    }
-  }
-}
-
-object ProductMarshaller extends Marshaller[Product] with LazyLogging {
-  override def marshallLineString(line: String): Product = {
-    line.split(CARREFOUR_HORIZONTAL_SEPARATOR) match {
-      case Array(productId, price) => Product(productId.toInt, price.toDouble)
-    }
-  }
-}
